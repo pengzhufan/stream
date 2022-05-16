9a33012249bae69b838161a0bd8d24b56ddf0ecc
pengzhufan <1165157407@qq.com>
2022-05-16 09:31:36 +0800

    提交测试数据

--- src/main/java/com/yjiewei/TestCode.java
+++ src/main/java/com/yjiewei/TestCode.java
@@ -22,7 +22,7 @@ public class TestCode {
     public void test1() {
 
         List<Book> bookList = new ArrayList<>();
-        List<Author> authorList = new ArrayList<>();
+        List<Author> authorList = getAuthors();
         Set<Book> uniqueBookValues = new HashSet<>();
         Set<Author> uniqueAuthorValues = new HashSet<>();
         for (Author author : authorList) {
@@ -78,5 +78,37 @@ public class TestCode {
         new Thread(() ->{
             System.out.println("我好想你*2");
         }).start();
+    }
+
+    private static List<Author> getAuthors() {
+        Author author1 = new Author(1L, "杨杰炜", "my introduction 1", 18, null);
+        Author author2 = new Author(2L, "yjw", "my introduction 2", 19, null);
+        Author author3 = new Author(3L, "yjw", "my introduction 3", 14, null);
+        Author author4 = new Author(4L, "wdt", "my introduction 4", 29, null);
+        Author author5 = new Author(5L, "wtf", "my introduction 5", 12, null);
+
+        List<Book> books1 = new ArrayList<>();
+        List<Book> books2 = new ArrayList<>();
+        List<Book> books3 = new ArrayList<>();
+
+        // 上面是作者和书
+        books1.add(new Book(1L, "类别,分类啊", "书名1", 45D, "这是简介哦"));
+        books1.add(new Book(2L, "高效", "书名2", 84D, "这是简介哦"));
+        books1.add(new Book(3L, "喜剧", "书名3", 83D, "这是简介哦"));
+
+        books2.add(new Book(5L, "天啊", "书名4", 65D, "这是简介哦"));
+        books2.add(new Book(6L, "高效", "书名5", 89D, "这是简介哦"));
+
+        books3.add(new Book(7L, "久啊", "书名6", 45D, "这是简介哦"));
+        books3.add(new Book(8L, "高效", "书名7", 44D, "这是简介哦"));
+        books3.add(new Book(9L, "喜剧", "书名8", 81D, "这是简介哦"));
+
+        author1.setBookList(books1);
+        author2.setBookList(books2);
+        author3.setBookList(books3);
+        author4.setBookList(books3);
+        author5.setBookList(books2);
+
+        return new ArrayList<>(Arrays.asList(author1, author2, author3, author4, author5));
     }
 }
